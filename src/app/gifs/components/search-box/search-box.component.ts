632865c79e4401bc312c0d65a85e812e4936d1cc
Elena Arocena <elena.arocena@innoway.es>
2024-01-09 12:33:09 +0100

    feat(home-page): add service for search and keep element

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class SearchBoxComponent {
 
+@ViewChild('txtTagInput')
+
+public tagInput!: ElementRef<HTMLInputElement>
+
+constructor(private gifsService: GifsService){}
+
+searchTag(){
+  const newTag = this.tagInput.nativeElement.value;
+  console.log({newTag});
+  this.gifsService.searchTag(newTag);
+  this.tagInput.nativeElement.value = '';
+}
 }
